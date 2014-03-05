3a6e2893d75fd4d1eb249a9dcf776b34b99a71fa
Gregory Montoir <cyx@users.sourceforge.net>
2014-03-05 15:24:01 +0800

    move TooDC descrambler to Pak.cpp

--- pak.cpp
+++ pak.cpp
@@ -1,7 +1,22 @@
 
 #include "pak.h"
 
-extern uint8_t *decode_toodc(uint8_t *p, int count);
+static const uint32_t XOR_KEY2 = 0x22683297;
+
+static uint8_t *decode_toodc(uint8_t *p, int count) {
+	uint32_t key = XOR_KEY2;
+	uint32_t acc = 0;
+	for (int i = 0; i < count; ++i) {
+		uint8_t *q = p + i * 4;
+		const uint32_t data = READ_LE_UINT32(q) ^ key;
+		uint32_t r = (q[2] + q[1] + q[0]) ^ q[3];
+		r += acc;
+		key += r;
+		acc += 0x4D;
+		WRITE_LE_UINT32(q, data);
+	}
+	return p + 4;
+}
 
 const char *Pak::FILENAME = "Pak01.pak";
 
