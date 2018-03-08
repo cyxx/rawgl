7ccc51fa6d0ec68834185ff269c5815888cfa623
Gregory Montoir <cyx@users.sourceforge.net>
2018-03-08 22:48:18 +0800

    convert_3do: add error messages if .ISO file format does not match

--- tools/convert_3do/opera.cpp
+++ tools/convert_3do/opera.cpp
@@ -25,46 +25,48 @@ static void dumpGameData(const char *name, FILE *in, uint32_t offset, uint32_t s
 static const int BLOCK_SIZE = 2048;
 
 static void readIso(FILE *fp, int block, int flags) {
-	if (block < 0) {
-		uint8_t buf[128];
-		fread(buf, sizeof(buf), 1, fp);
-		if (buf[0] == 1 && memcmp(buf + 40, "CD-ROM", 6) == 0) {
-			const int offset = readUint32BE(buf + 100);
-			readIso(fp, offset, 0);
-		}
-	}  else {
-		uint32_t attr = 0;
+	uint32_t attr = 0;
+	do {
+		fseek(fp, block * BLOCK_SIZE + 20, SEEK_SET);
 		do {
-			fseek(fp, block * BLOCK_SIZE + 20, SEEK_SET);
-			do {
-				uint8_t buf[72];
-				fread(buf, sizeof(buf), 1, fp);
-				attr = readUint32BE(buf);
-				const char *name = (const char *)buf + 32;
-				const uint32_t count = readUint32BE(buf + 64);
-				const uint32_t offset = readUint32BE(buf + 68);
-				fseek(fp, count * 4, SEEK_CUR);
-				switch (attr & 255) {
-				case 2:
-					if (flags & 1) {
-						const int pos = ftell(fp);
-						dumpGameData(name, fp, offset * BLOCK_SIZE, readUint32BE(buf + 16));
-						fseek(fp, pos, SEEK_SET);
-					}
-					break;
-				case 7:
-					if (strcmp(name, GAMEDATA_DIRECTORY) == 0) {
-						readIso(fp, offset, 1);
-					}
-					break;
+			uint8_t buf[72];
+			fread(buf, sizeof(buf), 1, fp);
+			attr = readUint32BE(buf);
+			const char *name = (const char *)buf + 32;
+			const uint32_t count = readUint32BE(buf + 64);
+			const uint32_t offset = readUint32BE(buf + 68);
+			fseek(fp, count * 4, SEEK_CUR);
+			switch (attr & 255) {
+			case 2:
+				if (flags & 1) {
+					const int pos = ftell(fp);
+					dumpGameData(name, fp, offset * BLOCK_SIZE, readUint32BE(buf + 16));
+					fseek(fp, pos, SEEK_SET);
 				}
-			} while (attr < 256);
-			++block;
-		} while ((attr >> 24) == 0x40);
-	}
+				break;
+			case 7:
+				if (strcmp(name, GAMEDATA_DIRECTORY) == 0) {
+					readIso(fp, offset, 1);
+				}
+				break;
+			}
+		} while (attr < 256);
+		++block;
+	} while ((attr >> 24) == 0x40);
 }
 
 int extractOperaIso(FILE *fp) {
-	readIso(fp, -1, 0);
+	uint8_t buf[128];
+	const int count = fread(buf, sizeof(buf), 1, fp);
+	if (count != sizeof(buf)) {
+		fprintf(stderr, "Failed to read %d bytes, ret %d\n", (int)sizeof(buf), count);
+		return -1;
+	}
+	if (buf[0] != 1 || memcmp(buf + 40, "CD-ROM", 6) != 0) {
+		fprintf(stderr, "Unexpected Opera ISO signature\n");
+		return -1;
+	}
+	const int offset = readUint32BE(buf + 100);
+	readIso(fp, offset, 0);
 	return 0;
 }
